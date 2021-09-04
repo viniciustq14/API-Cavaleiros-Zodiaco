8adfeba3260ddcda462e3c167898aeaf46fc2777
viniciustq14 <viniciustq14@bol.com.br>
2021-09-04 12:03:15 -0300

    Adiciona Novas Rotas
    Rota de adicionar cavaleiro
    Rota de editar cavaleiro
    Rota de excluir cavaleiro

--- src/routes/cavaleiros.ts
+++ src/routes/cavaleiros.ts
@@ -3,7 +3,10 @@ import cavaleiros from '../controllers/cavaleiros';
 
 const router = express.Router();
 
-router.get('/cavaleiros', cavaleiros.listar);
-router.get('/cavaleiro/:id', cavaleiros.encontrar);
+router.get('/cavaleiros', cavaleiros.listar.bind(cavaleiros));
+router.get('/cavaleiro/:id', cavaleiros.encontrar.bind(cavaleiros));
+router.post('/cavaleiros/novo', cavaleiros.adicionar.bind(cavaleiros));
+router.post('/cavaleiros/editar/:id', cavaleiros.editar.bind(cavaleiros));
+router.get('/cavaleiros/excluir/:id', cavaleiros.excluir.bind(cavaleiros));
 
 export = router;
